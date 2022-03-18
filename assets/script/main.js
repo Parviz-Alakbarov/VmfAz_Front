34cce267fdd49818111425e3a1431c5ee52e3f38
Parviz-Alakbarov <parvizja@code.edu.az>
2022-03-18 14:35:27 +0400

    init

---
+++ assets/script/main.js
@@ -0,0 +1,18 @@
+$(function(){
+	$('.nav_hamburger').click(function(){
+		$('.nav-res_item').removeClass('active');
+		$(this).toggleClass('open');
+		$('.nav_items-res').toggleClass('nav_active')
+	});
+	$('.nav_search-res').click(function () {
+		$('.nav_items-res').removeClass('nav_active')
+		$('.nav-res_item').toggleClass('active');
+	})
+	$(window).on('scroll', function (e) {
+		if (window.scrollY>120) {
+			$('#scrool-up').addClass("active");
+		}else{
+			$('#scrool-up').removeClass("active")
+		}
+	});
+});
